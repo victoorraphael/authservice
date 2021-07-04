d817b57ccce49f881d9277825e7a4612a4264dea
Victor Raphael <56162733+victoorraphael@users.noreply.github.com>
2021-07-04 11:04:07 -0300

    feat[db]: create db connection

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"github.com/miguelpragier/pgkebab"
+	"log"
+)
+
+var db *pgkebab.DBLink
+
+func dbConnection() error {
+	log.Println("connecting database ...")
+
+	cn := pgkebab.ConnStringDirect("postgres://postgres:postgres123@localhost:5432/authservice?sslmode=disable")
+
+	const (
+		connTimeout                        = 10
+		execTimeout                        = 10
+		connAttemptsMax                    = 10
+		connAttemptsMaxMinutes             = 10
+		secondsBetweenReconnectionAttempts = 10
+		debugPrint                         = true
+	)
+
+	opts := pgkebab.Options(cn, connTimeout, execTimeout, connAttemptsMax, connAttemptsMaxMinutes, secondsBetweenReconnectionAttempts, debugPrint)
+
+	_db, err := pgkebab.NewConnected(opts)
+
+	if err != nil {
+		log.Println("failed to connect database")
+		return err
+	}
+
+	db = _db
+
+	return nil
+}
 
 func main()  {
-	fmt.Println("Hello world")
+	if err := dbConnection(); err != nil {
+		log.Fatal(err)
+	}
 }
